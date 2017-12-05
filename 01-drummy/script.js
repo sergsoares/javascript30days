9371357986a42a9b6c86303a5959dd8b15411b3e
Sergio Soares <acad.sergio@gmail.com>
2017-12-05 17:23:59 -0300

    Clock with Javascript learning some CSS properties

--- 01-drummy/script.js
+++ 01-drummy/script.js
@@ -9,7 +9,7 @@ function registerEvents(){
 }
 
 function playSound(e){  
-     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 
     if (! audio) { return }
